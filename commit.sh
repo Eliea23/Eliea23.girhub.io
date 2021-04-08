798dd7e9a2e4af9cf93533db7e853a278ae69363
Eliea23 <mcziemer@cps.edu>
2021-04-08 19:46:50 +0000

    test commit

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "Eliea23"
+git config --global user.email "mcziemer@cps.edu"
+git commit -m "test commit"
+git remote add origin https://github.com/Eliea23/Eliea23.girhub.io.git 
+git push -u origin master
